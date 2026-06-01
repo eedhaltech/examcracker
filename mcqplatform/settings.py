ed2e9845514862e399a41d2acc1ec7dbbe9b7da2
eedhaltech <eedhaltech@gmail.com>
2026-06-02 00:41:24 +0530

    Change session engine from cache to database

--- mcqplatform/settings.py
+++ mcqplatform/settings.py
@@ -93,7 +93,7 @@ CACHES = {
     }
 }
 
-SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 SESSION_CACHE_ALIAS = 'default'
 
 AUTH_PASSWORD_VALIDATORS = [
