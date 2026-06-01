f9a67364c2b6a1415e4ba73c9578bdd8e11f11e7
eedhaltech <eedhaltech@gmail.com>
2026-06-02 00:40:10 +0530

    Update settings.py

--- mcqplatform/settings.py
+++ mcqplatform/settings.py
@@ -77,16 +77,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mcqplatform.wsgi.application'
 
 # Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': config('DB_NAME', default='mcqplatform'),
-#         'USER': config('DB_USER', default='mcquser'),
-#         'PASSWORD': config('DB_PASSWORD', default='mcqpassword'),
-#         'HOST': config('DB_HOST', default='localhost'),
-#         'PORT': config('DB_PORT', default='5432'),
-#     }
-# }
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
