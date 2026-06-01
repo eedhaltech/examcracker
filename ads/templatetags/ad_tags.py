ec25a5beead285ea4eabb07afac408de21cbd49e
eedhaltech <eedhaltech@gmail.com>
2026-06-02 04:12:09 +0530

    Update ad_tags.py

--- ads/templatetags/ad_tags.py
+++ ads/templatetags/ad_tags.py
@@ -1,10 +1,12 @@
 from django import template
+from django.template.loader import render_to_string
+from django.utils.safestring import mark_safe
 from ads.models import PromoAd
 
 register = template.Library()
 
 
-@register.inclusion_tag('ads/promo_ad.html', takes_context=True)
+@register.simple_tag(takes_context=True)
 def render_ad(context, position):
     request = context.get('request')
     is_member = getattr(request, 'is_member', False) if request else False
@@ -22,4 +24,9 @@ def render_ad(context, position):
         if not ad:
             ad = qs.order_by('?').first()
 
-    return {'ad': ad, 'is_member': is_member, 'position': position}
+    rendered = render_to_string(
+        'ads/promo_ad.html',
+        {'ad': ad, 'is_member': is_member, 'position': position},
+        request=request
+    )
+    return mark_safe(rendered)
